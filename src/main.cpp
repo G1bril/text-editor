7754ed08fc9f97876bbe57582c3121549efb8130
G1bril <igov@outlook.fr>
2024-10-28 22:52:32 +0100

    file read upgrade & line count (kindof)

--- src/main.cpp
+++ src/main.cpp
@@ -3,28 +3,48 @@
 #include <string>
 #include <vector>
 
-class TextEditor {
-    public:
-    void OpenFile(std::string filename){
-        std::ifstream myfile (filename);
-        if (!myfile.is_open()) {
-            std::cout << "Failed to open the file" << std::endl;
+class TextEditor
+{
+public:
+    void OpenFile(std::string filename)
+    {
+
+        std::ifstream openedFile(filename);
+        // checks if the file name is correct
+        if (!openedFile.is_open())
+        {
+            std::cerr << "Failed to open the file" << std::endl;
         }
-        
-        else {
-            std::string mystring;   
-            myfile >> mystring;
-            std::cout << mystring;
+
+        else
+        {
+            // open file and copy it's content into a vector
+            std::vector<std::string> lines;
+            std::string buffer; // super cool variable name that makes me look like a cool cpp prgmer
+            while (std::getline(openedFile, buffer))
+            {
+                lines.push_back(buffer);
+            }
+            openedFile.close();
+            // Print vector elements using a loop
+            for (int i = 0; i < lines.size(); i++)
+                std::cout << i << ". " << lines[i] << std::endl;
         }
+
+        /*openedFile >> fileData;
+        std::cout << fileData;*/
     }
+}
 
-};
+;
 
-int main(){
+int main()
+{
     TextEditor text;
     std::string filename;
     std::cout << "Please type file name: " << std::endl;
     std::cin >> filename;
-    std::cout << filename << std::endl;
+    std::cout << filename << "Your file name is: " << std::endl; // debug purpose
     text.OpenFile(filename);
+    return 0;
 }
