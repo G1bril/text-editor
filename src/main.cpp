f4e375bf4333723d38a6b0f7d0b5e59cbc57259f
G1bril <igov@outlook.fr>
2024-10-28 15:19:08 +0100

    read text file prototype

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,30 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+class TextEditor {
+    public:
+    void OpenFile(std::string filename){
+        std::ifstream myfile (filename);
+        if (!myfile.is_open()) {
+            std::cout << "Failed to open the file" << std::endl;
+        }
+        
+        else {
+            std::string mystring;   
+            myfile >> mystring;
+            std::cout << mystring;
+        }
+    }
+
+};
 
 int main(){
-    std::cout << "Ciao" << std::endl;
+    TextEditor text;
+    std::string filename;
+    std::cout << "Please type file name: " << std::endl;
+    std::cin >> filename;
+    std::cout << filename << std::endl;
+    text.OpenFile(filename);
 }
