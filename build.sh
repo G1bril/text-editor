5d03bbeb90136a0d66251f46fb4c4779355974e4
G1bril <igov@outlook.fr>
2024-10-29 13:16:25 +0100

    added tests only, now adding everything (oops)

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+#using a shell script because i'm too lazy to do otherwise for now
 #!/bin/bash
 g++ -std=c++17 src/main.cpp -o build/text_editor
 echo "Build complete. Run the program with ./build/text_editor"
--- src/main.cpp
+++ src/main.cpp
@@ -2,18 +2,28 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <filesystem>
 
 class TextEditor
 {
 public:
     void OpenFile(std::string filename)
     {
-
+        // creates two objects for the filepath and the currently opened file
+        std::filesystem::path filepath(filename);
         std::ifstream openedFile(filename);
         // checks if the file name is correct
         if (!openedFile.is_open())
         {
             std::cerr << "Failed to open the file" << std::endl;
+        }
+        // check file extension. Can be done in a little less lazy way.
+        else if (!(filepath.extension() == ".txt" || filepath.extension() == ".md" ||
+                   filepath.extension() == ".csv" || filepath.extension() == ".json" ||
+                   filepath.extension() == ".xml" || filepath.extension() == ".html" ||
+                   filepath.extension() == ".log"))
+        {
+            std::cout << "Unsupported file extension" << std::endl;
         }
 
         else
@@ -28,7 +38,11 @@ public:
             openedFile.close();
             // Print vector elements using a loop
             for (int i = 0; i < lines.size(); i++)
+            {
                 std::cout << i << ". " << lines[i] << std::endl;
+            }
+            openedFile.close();
+            std::cout << "File Successfully closed" << std::endl;
         }
 
         /*openedFile >> fileData;
@@ -44,7 +58,7 @@ int main()
     std::string filename;
     std::cout << "Please type file name: " << std::endl;
     std::cin >> filename;
-    std::cout << filename << "Your file name is: " << std::endl; // debug purpose
+    std::cout << "Your file name is: " << filename << std::endl; // debug purpose
     text.OpenFile(filename);
     return 0;
 }
